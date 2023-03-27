2a0ffd3058deb472878871823e696108aced426e
Óscar Merino Martínez <omm1002@alu.ubu.es>
2023-03-27 09:24:04 +0200

    Merge branch 'main' of https://github.com/oskitar04/p03.DependenciasDeEstado_2023L into main

--- src/p02/c02_2023L/IParque.java
+++
@@ -1,7 +0,0 @@
-package p02.c02_2023L;
-// Las interfaces no se implementan los metodos, solo se definen
-public interface IParque {
-	
-	public abstract void entrarAlParque(String puerta);
-
-}//Tiene que ser publico, por que si no no se puede acceder a ello
